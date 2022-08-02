997796a738d61c58dde699f60c97dfbea2e5d02b
Grzegorz Lasak <glasak@protonmail.com>
2022-08-02 16:38:24 +0200

    Added highlight JS library for nicer way of displaying code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import VueHighlightJS from 'vue3-highlightjs';
+import 'highlight.js/styles/github.css';
 import router from './router';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'animate.css';
 import './assets/scss/app.scss';
+import App from './App.vue';
+import 'highlight.js/styles/default.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueHighlightJS)
+  .mount('#app');
